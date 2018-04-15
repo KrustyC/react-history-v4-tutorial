a2b8a2b74daf37a325a08886dcab2ec54ce95d99
Davide <davide.crestini94@gmail.com>
2018-04-15 09:39:41 +0100

    added extract plugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge')
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
@@ -25,6 +26,7 @@ module.exports = merge(common, {
       'process.env': {
         BASE_URL: JSON.stringify('/react-router-v4-tutorial')
       }
-    })
+    }),
+    new ExtractTextPlugin('[name].css')
   ]
 })
