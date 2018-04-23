3f8f75819bedd1899fa05088dd3ec7c55887e2b7
Davide <davide.crestini94@gmail.com>
2018-04-23 21:20:14 +0100

    fixed linting

--- src/views/Routes.jsx
+++ src/views/Routes.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { PrivateRoute } from 'routes-templates'
 
 import Home from './scenes/home'
 import Docs from './scenes/docs'
